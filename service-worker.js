8c6d4a017f9437793a284b8fa37406e937292bea
art-full <98484921+art-full@users.noreply.github.com>
2025-07-20 00:32:27 +0000

    Re-upload Site

--- service-worker.js
+++ service-worker.js
@@ -5,13 +5,14 @@ self.addEventListener('install', function(e) {
       return cache.addAll([
         './',
         './index.html',
+        './week2.html',
+        './week3.html',
         './manifest.json',
         './icon.png'
       ]);
     })
   );
 });
-
 self.addEventListener('fetch', function(e) {
   e.respondWith(
     caches.match(e.request).then(function(response) {
